292f614038687144d87eaf4027625ef9166a1cb9
Administrator <eladr8520@gmail.com>
2021-09-30 15:05:36 +0300

    new

--- first_package/practice.py
+++ first_package/practice.py
@@ -1,3 +1,3 @@
 name=input("enter your name: ")
 age=int(input("enter your age: "))
-print(f"{name} you will be 100 years old in {100-age} years old")
+print(f"{name} you will be 100 years old in {90-age} years old")
