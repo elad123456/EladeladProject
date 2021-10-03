d9785f7f00976197e258a15011b04905514d354f
Administrator <eladr8520@gmail.com>
2021-10-03 10:32:04 +0300

    new

---
+++ first_package/checkif.py
@@ -0,0 +1,5 @@
+grade=100-1000
+if 0>grade or grade>100:
+    print("invalied")
+else:
+    print("OK")
--- first_package/first_file.py
+++ first_package/first_file.py
@@ -3,4 +3,4 @@ b = int(input("enter num 2:"))
 c = int(input("enter num 3:"))
 sum = a+b+c
 print(f"sum: {sum}   avarage: {int(sum/3)}")
-print(int(sum/3))
+print(sum//3)
